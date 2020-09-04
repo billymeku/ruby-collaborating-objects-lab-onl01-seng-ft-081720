a7534fa144a7e1bb749a03ab9aad714612d21f73
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-04 20:54:38 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-=======
-# class MP3Importer
-  
-#   attr_accessor :path
-
-#   def initialize(path)
-#     @path = path
-#   end
-
-#   def files
-#     @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
-#   end
-
-#   def import
-#     files.each {|song| Song.new_by_filename(song)}
-#   end
-
-  
-# end 
-
-
 class MP3Importer
 
   attr_accessor :name, :path
@@ -36,11 +14,7 @@ class MP3Importer
     end
     @@all
   end
-
-<<<<<<< HEAD
   
-
->>>>>>> 7eb16821eea26e3072452e4abffc9342e42482dd
  def import
     files.each {|song| Song.new_by_filename(song)}
   end
